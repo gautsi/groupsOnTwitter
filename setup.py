c97c19cc64eaa4b9e7b060d7557e53f296f1736b
Gautam <gautam.sisodia@gmail.com>
2014-09-29 23:46:18 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "groupsontwitter",
+    version = "1.0",
+    packages = ["graphtools"],
+    author = "Gautam Sisodia",
+    author_email = "gautam.sisodia@gmail.com",
+    description = "A package of tools to measure hierarchy in directed graphs.",
+    url = "https://github.com/gautsi/groupsOnTwitter"
+)
